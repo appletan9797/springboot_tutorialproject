05ab1ddfd9b1f04af7efbfba61958f04bcb75128
Apple <appleteng97@gmail.com>
2022-09-26 00:13:00 +0800

    Change DB connection from H2 to MySQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,8 +7,8 @@
  * Created: 22 Sep 2022
  */
 
-insert into ALIEN values (101,'Apple','Java');
-insert into ALIEN values (102,'Branda','Android');
-insert into ALIEN values (103,'Chloe','Python');
-insert into ALIEN values (104,'Dennis','Java');
-insert into ALIEN values (105,'Emma','Android');
+insert into alien values (101,'Apple','Java');
+insert into alien values (102,'Branda','Android');
+insert into alien values (103,'Chloe','Python');
+insert into alien values (104,'Dennis','Java');
+insert into alien values (105,'Emma','Android');
